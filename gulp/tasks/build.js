6e194cc4c9298038274b5c7354b2c6beadf0d372
shimakyohsuke <shimakyohsuke@gmail.com>
2017-03-07 19:05:26 +0900

    package update

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,5 +1,21 @@
 'use strict'
 
 import gulp from 'gulp'
+import runSequence from 'run-sequence'
+const notifier = require('node-notifier')
 
-gulp.task('build', ['html', 'stylus', 'webpack', 'images'])
+gulp.task('notify', () => {
+  return notifier.notify({
+    'title': 'gulp tasks',
+    'message': 'Build Complete!!'
+  })
+})
+
+gulp.task('build', (callback) => {
+  return runSequence(
+      'images',
+      ['html', 'stylus', 'webpack'],
+      'notify',
+      callback
+    )
+})
